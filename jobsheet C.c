688f0869be1f7f8e8eb0fd7c1c5a9e733561bf38
nurfajrin20 <65478260+nurfajrin20@users.noreply.github.com>
2020-05-17 12:39:10 +0700

    Create jobsheet C.c
    menampilkan nimmasing-masing mahasiswa dengan bahasa c

---
+++ jobsheet C.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main(){
+int nim[11]={1,2,3,4,5,6,7,8,9,10,11};
+
+printf("Nim ke-1=%\n",nim[0]);
+printf("Nim ke-2=%\n",nim[1]);
+printf("Nim ke-3=%\n",nim[2]);
+printf("Nim ke-4=%\n",nim[3]);
+printf("Nim ke-5=%\n",nim[4]);
+printf("Nim ke-6=5%n",nim[5]);
+printf("Nim ke-7=6%n",nim[6]);
+printf("Nim ke-8=7%n",nim[7]);
+printf("Nim ke-9=8%n",nim[8]);
+printf("Nim ke-10=9%n",nim[9]);
+printf("Nim ke-11=10%n",nim[10]);
+}
