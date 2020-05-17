f467864d129569b31b7ee75e9395feb3ee110a52
nurfajrin20 <65478260+nurfajrin20@users.noreply.github.com>
2020-05-17 12:28:50 +0700

    Create jobsheadt 1C++.cpp
    menampilkan nim masing-masing mahasiswa

---
+++ jobsheadt 1C++.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{	
+	cout<<"NURFAJRIN-FIB019111-Kelompok 22\n\n";
+	int nim[11]={11,10,9,8,7,6,5,4,3,2,1};
+	cout<<"Nim ke-1 adalah"<< nim[0]<<endl;
+	cout<<"Nim ke-2 adalah"<< nim[1]<<endl;
+	cout<<"Nim ke-3 adalah"<< nim[2]<<endl;
+	cout<<"Nim ke-4 adalah"<< nim[3]<<endl;
+	cout<<"Nim ke-5 adalah"<< nim[4]<<endl;
+	cout<<"Nim ke-6 adalah"<< nim[5]<<endl;
+	cout<<"Nim ke-7 adalah"<< nim[6]<<endl;
+	cout<<"Nim ke-8 adalah"<< nim[7]<<endl;
+	cout<<"Nim ke-9 adalah"<< nim[8]<<endl;
+	cout<<"Nim ke-10 adalah"<< nim[9]<<endl;
+	cout<<"Nim ke-11 adalah"<< nim[10]<<endl;	
+}
