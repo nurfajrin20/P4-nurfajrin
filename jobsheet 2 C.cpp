e1a44f4005b292199c580609b58dc55ff7a5d24d
nurfajrin20 <65478260+nurfajrin20@users.noreply.github.com>
2020-05-17 12:43:27 +0700

    Create jobsheet 2 C.cpp
    Program mengisi dan menampilkan elemen array dengan index array sesuai dengan NIM terakhir setiap mahasiswa.

---
+++ jobsheet 2 C.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+	int x [11];
+	cout<<"Masukkan nilai 1:";
+	cin>>x[0];
+	cout<<"Masukkan nilai 2:";
+	cin>>x[1];
+	cout<<"Masukkan nilai 3:";
+	cin>>x[2];
+	cout<<"Masukkan nilai 4:";
+	cin>>x[3];
+	cout<<"Masukkan nilai 5:";
+	cin>>x[4];
+	cout<<"Masukkan nilai 6:";
+	cin>>x[5];
+	cout<<"Masukkan nilai 7:";
+	cin>>x[6];
+	cout<<"Masukkan nilai 8:";
+	cin>>x[7];
+	cout<<"Masukkan nilai 9:";
+	cin>>x[8];
+	cout<<"Masukkan nilai 10:";
+	cin>>x[9];
+	cout<<"Masukkan nilai 11:";
+	cin>>x[10];
+	cout<<endl;
+	cout<<"Nilai ke-1="<<x[0]<<endl;
+	cout<<"Nilai ke-2="<<x[1]<<endl;
+	cout<<"Nilai ke-3="<<x[2]<<endl;
+	cout<<"Nilai ke-4="<<x[3]<<endl;
+	cout<<"Nilai ke-5="<<x[4]<<endl;
+	cout<<"Nilai ke-6="<<x[5]<<endl;
+	cout<<"Nilai ke-7="<<x[6]<<endl;
+	cout<<"Nilai ke-8="<<x[7]<<endl;
+	cout<<"Nilai ke-9="<<x[8]<<endl;
+	cout<<"Nilai ke-10="<<x[9]<<endl;
+	cout<<"Nilai ke-11="<<x[10]<<endl;
+	system("PAUSE");
+}
